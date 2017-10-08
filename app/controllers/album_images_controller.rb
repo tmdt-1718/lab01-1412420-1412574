51a23f35994e58a9a0aabe2b5556e5ec80c2670c
Huuphuoc <phanphuocdt@gmail.com>
2017-10-08 14:45:05 +0700

    fix db sample, fix breadcumb

--- app/controllers/album_images_controller.rb
+++ app/controllers/album_images_controller.rb
@@ -1,8 +1,11 @@
 class AlbumImagesController < ApplicationController
   before_action :_set_album, :_increment_view_image, only: [:index]
+  add_breadcrumb "Albums", :albums_path  
+  
 
   def index 
     @images = @album.album_images.order(updated_at: :desc).page(params[:page]).per(ITEMS_PER_PAGE)
+    add_breadcrumb @album.id, album_album_images_path(album_id: @album.id)
   end 
 
   private 
